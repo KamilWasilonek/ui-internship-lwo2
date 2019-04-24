844be66eb34998574a55212e47268540d1f5cf7c
kwasilonek <kwasilonek@kwasilonek-ws-krakow.hw.griddynamics.net>
2019-04-24 19:00:52 +0200

    Changed if to switch in task 1

--- tasks/1_golf-score/golf-score.js
+++ tasks/1_golf-score/golf-score.js
@@ -11,17 +11,20 @@ export const GOLF_SCORES = {
 export function golfScore(par, strokes) {
   if (strokes === 1) {
     return GOLF_SCORES.HOLE_IN_ONE;
-  } else if (strokes <= par - 2) {
-    return GOLF_SCORES.EAGLE;
-  } else if (strokes === par - 1) {
-    return GOLF_SCORES.BIRDIE;
-  } else if (strokes === par) {
-    return GOLF_SCORES.PAR;
-  } else if (strokes === par + 1) {
-    return GOLF_SCORES.BOGEY;
-  } else if (strokes === par + 2) {
-    return GOLF_SCORES.DOUBLE_BOGEY;
-  } else if (strokes >= par + 3) {
-    return GOLF_SCORES.GO_HOME;
+  }
+  switch (strokes - par) {
+    case -2:
+    case -3:
+      return GOLF_SCORES.EAGLE;
+    case -1:
+      return GOLF_SCORES.BIRDIE;
+    case 0:
+      return GOLF_SCORES.PAR;
+    case 1:
+      return GOLF_SCORES.BOGEY;
+    case 2:
+      return GOLF_SCORES.DOUBLE_BOGEY;
+    default:
+      return GOLF_SCORES.GO_HOME;
   }
 }
