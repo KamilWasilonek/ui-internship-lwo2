f57d9e7768a33d7b51357c0bb484c34df4ea26ac
kwasilonek <kwasilonek@kwasilonek-ws-krakow.hw.griddynamics.net>
2019-04-24 17:24:14 +0200

    Changed task 10

--- tasks/10_remove-whitespaces/remove-whitespaces.js
+++ tasks/10_remove-whitespaces/remove-whitespaces.js
@@ -1,4 +1,5 @@
 export function trimWhiteSpaces(textToTrim) {
-  let text = textToTrim.split(' ');
-  return text.join('');
+  // let text = textToTrim.split(' ');
+  // return text.join('');
+  return textToTrim.replace(/\s/g, '');
 }
