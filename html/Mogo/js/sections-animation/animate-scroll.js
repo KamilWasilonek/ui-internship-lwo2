c85c676635f71539b26b29c612908835646448ab
Kamil Wasilonek <kwasilonek@kwasilonek-ws-krakow.hw.griddynamics.net>
2019-07-17 14:37:51 +0200

    Mogo sections animation

---
+++ html/Mogo/js/sections-animation/animate-scroll.js
@@ -0,0 +1,33 @@
+import {components} from './menu-elements.js';
+import {helperFunctions} from './helper-functions.js';
+
+const {navbarLogo, navbarMenu, navbarMobileButton} = components;
+
+const {
+  setNavbarBackground,
+  changeNavbarBackground,
+  addScrollEffect,
+  addEventToMenuItems,
+} = helperFunctions;
+
+document.addEventListener('DOMContentLoaded', () => {
+  changeNavbarBackground();
+  addEventToMenuItems();
+});
+
+navbarMobileButton.addEventListener('click', () => {
+  if (window.scrollY < 200) {
+    setNavbarBackground();
+  }
+
+  navbarMenu.classList.toggle('navbar__menu--active');
+});
+
+window.addEventListener('scroll', () => {
+  changeNavbarBackground();
+});
+
+navbarLogo.addEventListener('click', function(event) {
+  event.preventDefault();
+  addScrollEffect(event.target);
+});
---
+++ html/Mogo/js/sections-animation/menu-elements.js
@@ -0,0 +1,6 @@
+export const components = {
+  navbar: document.querySelector('.navbar'),
+  navbarMenu: document.querySelector('.navbar__menu'),
+  navbarMobileButton: document.querySelector('.navbar__mobile-btn'),
+  navbarLogo: document.querySelector('.navbar__logo'),
+};
