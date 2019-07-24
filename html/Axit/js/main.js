948bd705ad1bf86ad0b7d3f85834c92dd7fb3e06
Kamil Wasilonek <kwasilonek@kwasilonek-ws-krakow.hw.griddynamics.net>
2019-07-24 15:24:40 +0200

    Axit tab switching

--- html/Axit/js/main.js
+++ html/Axit/js/main.js
@@ -1,8 +1,3 @@
-/* eslint-disable max-len */
 import {addTabSwitchingEffect} from './tabs/tab-switching.js';
-// import {addBannerFormValidation} from './validation/banner-form-validation.js';
-// import {addContactFormValidation} from './validation/contact-form-validation.js';
 
-// addBannerFormValidation();
-// addContactFormValidation();
 addTabSwitchingEffect();
