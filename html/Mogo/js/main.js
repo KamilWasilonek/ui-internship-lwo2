cb5f26192ab8c1fb2bcdcd2e5fd4b3e807554380
Kamil Wasilonek <kwasilonek@kwasilonek-ws-krakow.hw.griddynamics.net>
2019-07-29 17:41:10 +0200

    Changed scroll animation

--- html/Mogo/js/main.js
+++ html/Mogo/js/main.js
@@ -1,6 +1,4 @@
-// import './accordion/accordion.js';
 import {runMenuAnimation} from './sections-animation/animate-scroll.js';
-// import {runSlideshow} from './slideshow/slideshow.js';
 
 runMenuAnimation();
-// runSlideshow();
+
--- html/Mogo/js/sections-animation/helper-functions.js
+++ html/Mogo/js/sections-animation/helper-functions.js
@@ -1,62 +1,53 @@
-import {components} from './navbar-components.js';
+let boxToHightlight;
+export function setNavbarBackground(navbar) {
+  navbar.classList.add('navbar--with-bg');
+}
 
-components.then((response) => {
-  const {navbar, navbarMenu} = response;
+function removeNavbarBackground(navbar) {
+  navbar.classList.remove('navbar--with-bg');
+}
 
-  document.addEventListener('DOMContentLoaded', () => {
-    function setNavbarBackground() {
-      navbar.classList.add('navbar--with-bg');
-    }
+export function removeMenuItemBackground() {
+  if (boxToHightlight) {
+    boxToHightlight.classList.remove('navbar__menu-item--active');
+  }
+}
 
-    function removeNavbarBackground() {
-      navbar.classList.remove('navbar--with-bg');
-    }
+export function changeNavbarBackground(navbar, navbarMenu) {
+  if (window.scrollY >= 200) {
+    setNavbarBackground(navbar);
+  } else if (!navbarMenu.classList.contains('navbar__menu--active')) {
+    removeNavbarBackground(navbar);
+  }
+}
 
-    function changeNavbarBackground() {
-      if (window.scrollY >= 200) {
-        setNavbarBackground();
-      } else if (!navbarMenu.classList.contains('navbar__menu--active')) {
-        removeNavbarBackground();
-      }
-    }
-
-    function addScrollEffect(event) {
-      if (event.getAttribute('href') === '#') {
-        return;
-      }
-      const elementName = event.getAttribute('href');
-      const element = document.querySelector('#' + elementName);
-      window.scrollTo({
-        behavior: 'smooth',
-        left: 0,
-        top: element.offsetTop,
-      });
-    }
-
-    function addEventToMenuItems() {
-      let boxToHightlight;
-      navbarMenu.addEventListener('click', (event) => {
-        event.preventDefault();
-        if (boxToHightlight) {
-          boxToHightlight.style.background = 'none';
-        }
-        let target = event.target;
-        if (target.tagName === 'A') {
-          addScrollEffect(event.target);
-          boxToHightlight = target.parentElement;
-        } else if (target.tagName === 'LI') {
-          addScrollEffect(event.target.firstElementChild);
-          boxToHightlight = target;
-        }
-        boxToHightlight.style.background = 'rgba(121, 121, 121, 0.8)';
-      });
-    }
-
-    resolve({
-      setNavbarBackground,
-      changeNavbarBackground,
-      addScrollEffect,
-      addEventToMenuItems,
-    });
+export function scrollToSection(eventTarget) {
+  if (eventTarget.getAttribute('href') === '#') {
+    return;
+  }
+  const elementName = eventTarget.getAttribute('href');
+  const element = document.querySelector('#' + elementName);
+  window.scrollTo({
+    behavior: 'smooth',
+    left: 0,
+    top: element.offsetTop,
   });
-});
+}
+
+export function addEventToMenuItems(navbarMenu) {
+  navbarMenu.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (boxToHightlight) {
+      boxToHightlight.classList.remove('navbar__menu-item--active');
+    }
+    let target = event.target;
+    if (target.tagName === 'A') {
+      scrollToSection(event.target);
+      boxToHightlight = target.parentElement;
+    } else if (target.tagName === 'LI') {
+      scrollToSection(event.target.firstElementChild);
+      boxToHightlight = target;
+    }
+    boxToHightlight.classList.add('navbar__menu-item--active');
+  });
+}
