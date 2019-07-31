d10c6431362a86eecf47421f15b794fa8ef58434
Kamil Wasilonek <kwasilonek@kwasilonek-ws-krakow.hw.griddynamics.net>
2019-07-31 13:15:22 +0200

    Changed accordion

--- html/Mogo/js/accordion/components.js
+++ html/Mogo/js/accordion/components.js
@@ -1,13 +1,22 @@
 export const components = new Promise((resolve) => {
   document.addEventListener('DOMContentLoaded', () => {
-    const accordion = document.getElementsByClassName('accordion');
-    const panelElements = document.getElementsByClassName('services__panel');
+    const accordionsList = document.getElementsByClassName('accordion');
+    const panelsList = document.getElementsByClassName('services__panel');
     const imgElement = document.querySelector('.services__img');
+    const accordionsContainer = document.querySelector('.services__container');
+
+    const images = [
+      './assests/images/services/img-0.jpg',
+      './assests/images/services/img-1.jpg',
+      './assests/images/services/img-2.jpg',
+    ];
 
     resolve({
-      accordion,
-      panelElements,
+      accordionsList,
+      panelsList,
       imgElement,
+      accordionsContainer,
+      images,
     });
   });
 });
