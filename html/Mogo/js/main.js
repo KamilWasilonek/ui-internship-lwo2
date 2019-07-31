4fde85b156ed0ef26da9807d9e1007c25b73faa3
Kamil Wasilonek <kwasilonek@kwasilonek-ws-krakow.hw.griddynamics.net>
2019-07-31 13:54:06 +0200

    Mogo slider

--- html/Mogo/js/main.js
+++ html/Mogo/js/main.js
@@ -1,5 +1,5 @@
-// import {runPeopleSlider} from './slideshow/people-slider/people-slider.js';
-// import {runMessageSlider} from './slideshow/message-slider/message-slider.js';
+import {runPeopleSlider} from './slideshow/people-slider/people-slider.js';
+import {runMessageSlider} from './slideshow/message-slider/message-slider.js';
 import {runMenuAnimation} from './sections-animation/animate-scroll.js';
 // import {addPopup} from './popup/popup.js';
 import {addAccordionEffect} from './accordion/accordion.js';
@@ -8,11 +8,8 @@ import {addAccordionEffect} from './accordion/accordion.js';
 
 // generateLatesPosts();
 // generateFooterPosts();
-// runPeopleSlider();
-// runMessageSlider();
+runPeopleSlider();
+runMessageSlider();
 runMenuAnimation();
 // addPopup();
 addAccordionEffect();
-
-
-
