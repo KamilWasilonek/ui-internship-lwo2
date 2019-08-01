73b7d989428ccafb642effc2347747a3235b084a
Kamil Wasilonek <kwasilonek@kwasilonek-ws-krakow.hw.griddynamics.net>
2019-08-01 10:37:54 +0200

    Merge with master

--- html/Axit/js/main.js
+++ html/Axit/js/main.js
@@ -1,3 +1,6 @@
-import {addTabSwitchingEffect} from './tabs/tab-switching.js';
+/* eslint-disable max-len */
+import {addContactFormValidation} from './validation/contact-validation/contact-form-validation.js';
+import {addRegistrationFormValidation} from './validation/registration-form-validation/registration-form-validation.js';
 
-addTabSwitchingEffect();
+addContactFormValidation();
+addRegistrationFormValidation();
