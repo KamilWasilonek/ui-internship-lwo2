64c608a92c8810b60b566893cb0dd9783d705207
Kamil Wasilonek <kwasilonek@kwasilonek-ws-krakow.hw.griddynamics.net>
2019-08-02 11:45:15 +0200

    Changed slider arrows, added animation

--- html/Mogo/js/slideshow/message-slider/message-slider.js
+++ html/Mogo/js/slideshow/message-slider/message-slider.js
@@ -1,34 +1,22 @@
 import {components} from './message-components.js';
-import {slide} from '../helper-functions.js';
+import {
+  slide,
+  moveRight,
+  moveLeft,
+} from '../helper-functions.js';
 
 export function runMessageSlider() {
-  components.then((response) => {
-    const {
-      messageSlides,
-      messageNextBtn,
-      messagePrevBtn,
-    } = response;
-
-    let messageCurrentSlide = 0;
+  components.then(({messageSlides, messageNextBtn, messagePrevBtn}) => {
+    let currentSlideId = 0;
 
     slide(messageSlides, 0);
 
     messageNextBtn.addEventListener('click', () => {
-      if (messageCurrentSlide === messageSlides.length - 1) {
-        messageCurrentSlide = 0;
-      } else {
-        messageCurrentSlide += 1;
-      }
-      slide(messageSlides, messageCurrentSlide);
+      currentSlideId = moveRight(messageSlides, currentSlideId);
     });
 
     messagePrevBtn.addEventListener('click', () => {
-      if (messageCurrentSlide === 0) {
-        messageCurrentSlide = messageSlides.length - 1;
-      } else {
-        messageCurrentSlide -= 1;
-      }
-      slide(messageSlides, messageCurrentSlide);
+      currentSlideId = moveLeft(messageSlides, currentSlideId);
     });
   });
 }
